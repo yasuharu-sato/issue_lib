a18e07268607a7d6161272293d09bbe437adf4f8
yasuharu-sato <yasuharu_sato@icloud.com>
2021-03-15 17:44:07 +0900

    Add problem code

---
+++ main.go
@@ -0,0 +1,25 @@
+package issue_lib
+
+import "fmt"
+
+type Issue struct {
+	Names []string
+	param *test
+}
+
+type test struct {
+	param string
+}
+
+func (i *Issue) Code() {
+	var anyString []string
+	for _, name := range i.Names {
+		anyString = append(anyString, name)
+	}
+	fmt.Printf("There is an issue(%s, %s).", anyString[0], i.param.param)
+}
+
+func main() {
+	var i Issue
+	i.Code()
+}
